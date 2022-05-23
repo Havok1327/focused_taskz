b5a59e5bbd3b9f03177ece15c6a687dbb3847e4d
Havok1327 <33668526+Havok1327@users.noreply.github.com>
2022-05-23 12:39:13 -0500

    Code clean up,and finalizing of app to publish to Playstore.

--- lib/widgets/dialog_widget.dart
+++ lib/widgets/dialog_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:intl/intl.dart';
+//import 'package:intl/intl.dart';
 
 import '../constants/constants.dart';
 import '../models/task.dart';
